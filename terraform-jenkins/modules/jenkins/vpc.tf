4be2592f4cae3c7d2e8eb103a88e6a8969dfc974
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-17 17:39:54 -0300

    adding resource for route table

--- terraform-jenkins/modules/jenkins/vpc.tf
+++ terraform-jenkins/modules/jenkins/vpc.tf
@@ -17,7 +17,21 @@ resource "aws_subnet" "this" {
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_subnet.this.id
+
   tags = {
     Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "this" {
+  default_route_table_id = aws_vpc.this.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
   }
 }
