597ae96f213f7f2aa24b4fbc2fd2fd31913f1298
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-17 17:59:16 -0300

    adding resource to create private subnets

--- terraform-jenkins/modules/jenkins/vpc.tf
+++ terraform-jenkins/modules/jenkins/vpc.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "this" {
     Name = "${var.env_prefix}-vpc"
   }
 }
+
+## Criando as Subnets PrivadaS (Private Subnet)
+resource "aws_subnet" "private_subnets" {
+  count      = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.this.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
