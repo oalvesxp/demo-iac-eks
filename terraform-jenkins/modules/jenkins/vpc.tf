20805b6d54c9faf36eb6fdca13d716982115d9a8
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-17 18:13:22 -0300

    adding resource to associating route table to public subnets

--- terraform-jenkins/modules/jenkins/vpc.tf
+++ terraform-jenkins/modules/jenkins/vpc.tf
@@ -55,3 +55,9 @@ resource "aws_route_table" "this" {
     Name = "${var.env_prefix}-rt"
   }
 }
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.this.id
+}
