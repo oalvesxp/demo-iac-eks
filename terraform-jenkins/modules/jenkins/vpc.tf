5705164e9e972854daa35b1d84a8d8a70c54b13e
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-17 17:58:43 -0300

    restarting vpc file, adding resource to create vpc

--- terraform-jenkins/modules/jenkins/vpc.tf
+++ terraform-jenkins/modules/jenkins/vpc.tf
@@ -1,41 +1,8 @@
-## Criando a VPC
+## Criando a Virtual Private Network (VPC)
 resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr
+
   tags = {
     Name = "${var.env_prefix}-vpc"
-  }
-}
-
-## Criando a Subnet
-resource "aws_subnet" "this" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.azs[0]
-
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
-}
-
-## Criando o Internet Gateway
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_subnet.this.id
-
-  tags = {
-    Name = "${var.env_prefix}-igw"
-  }
-}
-
-## Criando a Route Table para VPC
-resource "aws_default_route_table" "this" {
-  default_route_table_id = aws_vpc.this.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-rtb"
   }
 }
