91fa422be24ddc1932386fbb0e0b3461c322c64e
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-17 18:11:12 -0300

    adding resource to create route table

--- terraform-jenkins/modules/jenkins/vpc.tf
+++ terraform-jenkins/modules/jenkins/vpc.tf
@@ -40,3 +40,18 @@ resource "aws_internet_gateway" "this" {
     Name = "${var.env_prefix}-igw"
   }
 }
+
+## Criando o Route Table (RT)
+# Route Table
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rt"
+  }
+}
