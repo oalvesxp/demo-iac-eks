de315574cd279437a7241b8951242bf23ee5973d
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-17 18:24:20 -0300

    adding resource to create a security groupe for ssh access

---
+++ terraform-jenkins/modules/jenkins/security_group.tf
@@ -0,0 +1,18 @@
+## Grupos de Segurança (Security Groups)
+resource "aws_security_group" "ssh-sg" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
