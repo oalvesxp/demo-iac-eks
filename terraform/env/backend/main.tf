6556bf0c3d3ca8d10e2d080e0861e9fb16cd9aad
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 11:24:22 -0300

    changing tfstate key on remote backend

--- terraform/env/backend/main.tf
+++ terraform/env/backend/main.tf
@@ -6,7 +6,7 @@ terraform {
   ################################################################
   backend "s3" {
     bucket          = "tf-state-831511089306-us-east-1"
-    key             = "tf-backend/terraform.tfstate"
+    key             = "backend/terraform.tfstate"
     region          = "us-east-1"
     dynamodb_table  = "locking-831511089306-us-east-1"
     encrypt         = true
