41b59cc2b0d1bc20f63d12e51bde5646ef6365dc
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 11:16:13 -0300

    adding backend external after running terraform apply

--- terraform/env/backend/main.tf
+++ terraform/env/backend/main.tf
@@ -1,4 +1,17 @@
 terraform {
+  ################################################################
+  ## Depois de rodar o 'terraform apply' (com o backend local)
+  ## você precisa descomentar essa sessão e rodar o 'terraform init' novamente
+  ## para trocar o backend local para o backend remoto
+  ################################################################
+  # backend "s3" {
+  #   bucket          = "tf-state-831511089306-us-east-1"
+  #   key             = "tf-backend/terraform.tfstate"
+  #   region          = "us-east-1"
+  #   dynamodb_table  = "locking-831511089306-us-east-1"
+  #   encrypt         = true
+  # }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
