23a81e17c99c7355b0fa3b1923fbd5fda89bb152
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 11:58:52 -0300

    adding value to private_subnet_cidrs

--- terraform/env/staging/main.tf
+++ terraform/env/staging/main.tf
@@ -3,6 +3,7 @@ module "infra" {
   source = "../../modules/infra"
 
   ## tfvars
-  env_prefix = "staging"
-  vpc_cidr   = "10.0.0.0/16"
+  env_prefix           = "staging"
+  vpc_cidr             = "10.0.0.0/16"
+  private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
