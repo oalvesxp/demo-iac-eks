c314f6c6a8135bf13e95d87b0deefc851f7daec1
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 11:25:04 -0300

    updating backend file for environment staging

--- terraform/env/staging/backend.tf
+++ terraform/env/staging/backend.tf
@@ -1,13 +1,14 @@
 terraform {
   ################################################################
-  ## Assumindo que o Bucket S3 e a tabela DynamoDB já foram configuradas
-  ## See ../../modules/main.tf
+  ## Assumindo que o backend já foi configurado pelo ../backend
+  ## se você não configurar o backend externo, haverá problemas 
+  ## na execução deste env, veja ../backend/main.tf
   ################################################################
   backend "s3" {
-    bucket         = "tf-state-831511089306-us-east-1"
-    key            = "tf-staging/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "locking-831511089306-us-east-1"
-    encrypt        = true
+    bucket          = "tf-state-831511089306-us-east-1"
+    key             = "staging/terraform.tfstate"
+    region          = "us-east-1"
+    dynamodb_table  = "locking-831511089306-us-east-1"
+    encrypt         = true
   }
 }
