811d7a9c2c81a0c5484e6ced96117e9302e6529d
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 13:57:13 -0300

    adding value to cluster_name

--- terraform/env/staging/main.tf
+++ terraform/env/staging/main.tf
@@ -8,4 +8,5 @@ module "infra" {
   azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnet_cidrs  = ["10.0.101.0/24", "10.0.102.0/24"]
+  cluster_name         = "demo-api"
 }
