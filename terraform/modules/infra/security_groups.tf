4e57a2a9863fa31c6f8d20432a1d8b8a0941e3da
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 13:01:26 -0300

    Merging branch 'staging' with all network setups done

--- terraform/modules/infra/security_groups.tf
+++ terraform/modules/infra/security_groups.tf
@@ -8,3 +8,22 @@ resource "aws_security_group" "ssh_eks" {
     Name = "${var.env_prefix}-ssh-eks-sg"
   }
 }
+
+# SSH Cluster - Regra de entrada (SSH Cluster Rule In)
+resource "aws_security_group_rule" "ssh_eks_in" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Enabled to any IP
+  security_group_id = aws_security_group.ssh_eks.id
+}
+
+resource "aws_security_group_rule" "ssh_eks_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] # Enabled to any IP
+  security_group_id = aws_security_group.ssh_eks.id
+}
