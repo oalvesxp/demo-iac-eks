c8bed6070141652a31ebac119f92d9920ebcbe60
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 22:55:31 -0300

    adding resource to create EKS Group Nodes

--- terraform/modules/infra/nodes.tf
+++ terraform/modules/infra/nodes.tf
@@ -38,3 +38,33 @@ resource "aws_iam_role_policy_attachment" "nodes_ssm_managed_instance_core" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.nodes.name
 }
+
+# Criando o grupo de nodes EKS (EKS Group Nodes)
+resource "aws_eks_node_group" "private" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "private-nodes"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    aws_subnet.private[0].id
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 5
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes_eks_worker_policy,
+    aws_iam_role_policy_attachment.nodes_eks_cni_policy,
+    aws_iam_role_policy_attachment.nodes_ec2_container_registry_policy,
+  ]
+}
