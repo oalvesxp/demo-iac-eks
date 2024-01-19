9957c34333638aeaff5611fb547317459312d534
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-18 22:21:33 -0300

    adding resource to attachmenting AmazonEKS_CNI_Policy to role EKSGroupNodes

--- terraform/modules/infra/nodes.tf
+++ terraform/modules/infra/nodes.tf
@@ -20,3 +20,9 @@ resource "aws_iam_role_policy_attachment" "nodes_eks_worker_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role = aws_iam_role.nodes.name
 }
+
+## Anexando a politica de 'EKS CNI' a role 'RoleForEKSGroupNodes'
+resource "aws_iam_role_policy_attachment" "nodes_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.nodes.name
+}
